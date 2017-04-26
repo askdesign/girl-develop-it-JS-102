c90601a15553203b9b0f1c0c1761f16b5686ed4c
Anne S. Katzeff <anne@askdesign.biz>
2017-04-26 12:57:46 -0400

    Create ask-homwork3.js
    OK, trying to add my files to our class 3 folder. Learning about GIT in the process.

---
+++ ask-homework3.js
@@ -0,0 +1,76 @@
+// https://openclipart.org/search/json/?query=elephants - query changed from unicorn
+
+// CLASS 3 CODE
+
+var elephants; // originally was unicorns; changed all unicorns in code to elephants
+
+function loadJSON(file, callback) {
+  var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+  	xobj.open('GET', file, true);
+  	xobj.onreadystatechange = function () {
+    	if (xobj.readyState == 4 && xobj.status == "200") {
+     // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+     callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+
+function getRandomThing(array) {
+  var randomNumberBetween0And1 = Math.random();
+  var highestNumber = array.length;
+  var randomInteger = Math.floor(
+    randomNumberBetween0And1 * highestNumber
+  );
+  return array[randomInteger];
+}
+
+function getImage() {
+  var img = document.createElement('img');
+  var elephant = getRandomThing(elephants);
+  img.src = elephant.svg.url;
+  return img;
+}
+
+function handleClick(event) {
+  if (event.target.childElementCount > 0) {
+    return;
+  }
+  var today = new Date().getDate();
+  if (event.target.innerHTML > today) {
+    return;
+  }
+  var img = getImage();
+  event.target.appendChild(img);
+}
+
+function getDay(num) {
+  var day = document.createElement('div');
+  day.classList.add('day');
+  day.innerHTML = num;
+  day.addEventListener('click', handleClick);
+  return day;
+}
+
+function drawCalendar() {
+  var container = document.getElementsByClassName('container')[0];
+  var year = new Date().getFullYear();
+  var month = new Date().getMonth() + 1;
+  var daysInMonth = new Date(year, month, 0).getDate();
+
+  for (var i = 1; i <= daysInMonth; i ++) {
+    var day = getDay(i);
+    container.appendChild(day);
+  }
+}
+
+function getElephantsThenDrawCalendar() { // originally getUnicornsThenDrawCalendar
+  loadJSON('elephantsData.json', function(data) { // originally 'unicornData.json'
+    elephants = JSON.parse(data).payload;
+    drawCalendar();
+  });
+}
+
+getElephantsThenDrawCalendar();
